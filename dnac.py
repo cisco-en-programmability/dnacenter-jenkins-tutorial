562968702e3a600a6004af01aca8ad8560856430
Jose Bogarin Solano <jbogarin@altus.cr>
2021-10-01 11:00:34 -0600

    Configuration template deployment

--- dnac.py
+++ dnac.py
@@ -1,13 +1,63 @@
 from dnacentersdk import api
+from datetime import datetime
+import time
+import csv
 
-# Create a DNACenterAPI connection object;
-# it uses DNA Center sandbox URL, username and password, with DNA Center API version 2.2.2.3.
-# and requests to verify the server's TLS certificate with verify=True.
 dnac = api.DNACenterAPI()
 
-# Find all devices that have 'Switches and Hubs' in their family
-devices = dnac.devices.get_device_list(family='Switches and Hubs')
+devices = dnac.devices.get_device_list(family="Switches and Hubs")
 
-# Print all of demo devices
 for device in devices.response:
-    print('{:20s}{}'.format(device.hostname, device.upTime))
+    print("{:20s} {}".format(device.hostname, device.upTime))
+
+TEMPLATE = ""
+with open('deploy/template.j2', 'r') as file:
+    TEMPLATE = file.read()
+
+today = datetime.today()
+TSTAMP = today.strftime("%Y-%m-%dT%H-%M-%S")
+print(TSTAMP)
+
+def deploy_template(device_name):
+    compliance_projects = dnac.configuration_templates.get_projects(name="Compliance")
+    print(f"Compliance Project ID is: {compliance_projects[0].id}")
+
+    device_types = [{"productFamily": "Routers"}, {"productFamily": "Switches and Hubs"}]
+    compliance_template_task = dnac.configuration_templates.create_template(
+        project_id = compliance_projects[0].id,
+        author="jenkins",
+        composite=False,
+        description=f"Jenkins deployment {TSTAMP}",
+        deviceTypes=device_types,
+        softwareType="IOS-XE",
+        softwareVariant="XE",
+        templateContent=TEMPLATE,
+        language="JINJA",
+        name=f"compliance-{device_name}-{TSTAMP}"
+    )
+
+    print(f"Compliance project task id is: {compliance_template_task.response.taskId}")
+    time.sleep(60)
+
+    task = dnac.task.get_task_by_id(compliance_template_task.response.taskId)
+    template_id = task.response.data
+    
+    dnac.configuration_templates.version_template(comments="Jenkins Deployment", templateId=template_id)
+
+    time.sleep(10)
+
+    deploy_template_task = dnac.configuration_templates.deploy_template_v2(
+        forcePushTemplate=True,
+        isComposite=False,
+        templateId=template_id,
+        targetInfo=[{"id": device_name, "type": "MANAGED_DEVICE_HOSTNAME"}]
+    )
+
+    task = dnac.task.get_task_by_id(deploy_template_task.response.taskId)
+    print(task)
+
+with open('deploy/hosts.csv', newline='') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+    next(csvreader)
+    for row in csvreader:
+        deploy_template(row[0])
