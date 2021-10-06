9b0a0a222632fe9278a6c8ec70e2c40f54971b27
Jose Bogarin Solano <jbogarin@altus.cr>
2021-10-06 09:24:01 -0600

    Included templates with variables/parms

--- dnac.py
+++ dnac.py
@@ -1,42 +1,70 @@
 from dnacentersdk import api
 from datetime import datetime
+import yaml
 import time
-import csv
+import sys
+import logging
+
+logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
 
 dnac = api.DNACenterAPI()
 
-devices = dnac.devices.get_device_list(family="Switches and Hubs")
-
-for device in devices.response:
-    print("{:20s} {}".format(device.hostname, device.upTime))
-
-TEMPLATE = ""
-with open('deploy/template.j2', 'r') as file:
-    TEMPLATE = file.read()
-
 today = datetime.today()
 TSTAMP = today.strftime("%Y-%m-%dT%H-%M-%S")
-print(TSTAMP)
 
-def deploy_template(device_name):
-    compliance_projects = dnac.configuration_templates.get_projects(name="Compliance")
-    print(f"Compliance Project ID is: {compliance_projects[0].id}")
+CONFIG = ""
+TEMPLATE = ""
+with open('deploy/dnac.yaml', 'r') as file:
+    CONFIG = yaml.safe_load(file)
 
-    device_types = [{"productFamily": "Routers"}, {"productFamily": "Switches and Hubs"}]
+def has_defined_variables(devices, variables):
+    """
+    Checks if the template has variables and if the devices has those variables defined
+    """
+    template_variables = []
+    for variable in variables:
+        template_variables.append(variable.get("parameterName"))
+    for device in devices:
+        if device.get("variables", None) is None:
+            return False
+        for variable in template_variables:
+            if device.get("variables").get(variable, None) is None:
+                return False
+    return True
+
+def deploy_template(project, project_id, has_variables):
+    devices = project.get("devices")
+    template_filename = project.get("file")
+    product_family = project.get("product_family")
+    device_types = []
+    for family in product_family:
+        device_types.append({"productFamily": family})
+
+    template_file = ""
+    with open(f"templates/{template_filename}", 'r') as file:
+        template_file = file.read()
+    template_params  = []
+    if has_variables:
+        template_params = project.get("variables")
     compliance_template_task = dnac.configuration_templates.create_template(
-        project_id = compliance_projects[0].id,
+        project_id = project_id,
         author="jenkins",
         composite=False,
         description=f"Jenkins deployment {TSTAMP}",
         deviceTypes=device_types,
         softwareType="IOS-XE",
         softwareVariant="XE",
-        templateContent=TEMPLATE,
+        templateContent=template_file,
         language="JINJA",
-        name=f"compliance-{device_name}-{TSTAMP}"
+        name=f"{project.get('name')}-{TSTAMP}",
+        templateParams=template_params
     )
 
-    print(f"Compliance project task id is: {compliance_template_task.response.taskId}")
+    logger.info(f"Compliance project {project.get('name')}-{TSTAMP} task id is: {compliance_template_task.response.taskId}")
+    logger.info("Sleeping for 60 seconds...")
     time.sleep(60)
 
     task = dnac.task.get_task_by_id(compliance_template_task.response.taskId)
@@ -44,20 +72,57 @@ def deploy_template(device_name):
     
     dnac.configuration_templates.version_template(comments="Jenkins Deployment", templateId=template_id)
 
+    logger.info("Sleeping for 10 seconds...")
     time.sleep(10)
+
+    target_info = []
+    for device in devices:
+        if has_variables:
+            target_info.append({"id": device.get("name"), 
+                                "type": "MANAGED_DEVICE_HOSTNAME",
+                                "params": device.get("variables")})
+        else:
+            target_info.append({"id": device.get("name"), "type": "MANAGED_DEVICE_HOSTNAME"})
 
     deploy_template_task = dnac.configuration_templates.deploy_template_v2(
         forcePushTemplate=True,
         isComposite=False,
         templateId=template_id,
-        targetInfo=[{"id": device_name, "type": "MANAGED_DEVICE_HOSTNAME"}]
+        targetInfo=target_info
     )
 
+    logger.info("Sleeping for 60 seconds...")
+    time.sleep(60)
     task = dnac.task.get_task_by_id(deploy_template_task.response.taskId)
-    print(task)
+    logger.info(f"Deployment status is: {task.response.progress}")
 
-with open('deploy/hosts.csv', newline='') as csvfile:
-    csvreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
-    next(csvreader)
-    for row in csvreader:
-        deploy_template(row[0])
+def get_project(name):
+    projects = dnac.configuration_templates.get_projects(name=name)
+    if len(projects) > 0:
+        return projects[0].id
+    project_task = dnac.configuration_templates.create_project(name=name)
+    time.sleep(5)
+    project_task_data = dnac.task.get_task_by_id(project_task.response.taskId)
+    return project_task_data.response.data
+
+def deploy_configuration_templates():
+    configuration_templates = CONFIG.get("configuration_templates", None)
+    for configuration_template in configuration_templates:
+        project_id = get_project(configuration_template["project"])
+        if configuration_template.get("variables"):
+            if has_defined_variables(configuration_template.get("devices"),
+                                     configuration_template.get("variables")):
+                logger.info("Deploying template with variables")
+                deploy_template(configuration_template, project_id, True)
+        else:
+            logger.info("Deploying template without variables")
+            deploy_template(configuration_template, project_id, False)
+
+def deploy_sites():
+    return
+
+
+if __name__ == "__main__":
+    # execute only if run as a script
+    deploy_configuration_templates()
+    deploy_sites()
