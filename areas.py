ef508e3b5c917a031d06a26a173cc6e9f72f208d
Jose Bogarin Solano <jbogarin@altus.cr>
2021-10-06 18:20:12 -0600

    Added site validation

---
+++ areas.py
@@ -0,0 +1,82 @@
+from dnacentersdk import api
+import yaml
+import sys
+import logging
+
+logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+dnac = api.DNACenterAPI()
+
+AREAS = ""
+with open('config/areas.yaml', 'r') as file:
+    AREAS = yaml.safe_load(file)
+
+def deploy_sites(sites):
+    area = {
+        "type": "area",
+        "site": {
+            "area": {
+                "name": "",
+                "parentName": ""
+            }
+        }
+    }
+    
+    building = {
+        "type": "building",
+        "site": {
+            "building": {
+                "name": "",
+                "parentName": "",
+                "latitude": 0.0,
+                "longitude":0.0 
+            }
+        }
+    }
+
+    floor = {
+        "type": "floor",
+        "site": {
+            "floor": {
+                "name": "",
+                "parentName": "",
+                "height": 0,
+                "length": 0,
+                "width": 0,
+                "rfModel": ""
+            }
+        }
+    }
+    
+    for site in sites:
+        if site["type"] == 'area':
+            area["site"]["area"]["name"] = site["name"]
+            area["site"]["area"]["parentName"] = site["parentName"]
+            logger.info("Deploying area...")
+            try:
+                dnac_response = dnac.sites.get_site(name=f'{site.get("parentName")}/{site.get("name")}')
+                if len(dnac_response.response):
+                    logger.error(f'Site {site.get("parentName")}/{site.get("name")} already exists')
+                    sys.exit(1)
+                dnac.sites.create_site(payload=area)
+            except:
+                dnac.sites.create_site(payload=area)
+        elif site["type"] == 'building':
+            building["site"]["building"]["name"] = site["name"]
+            building["site"]["building"]["parentName"] = site["parentName"]
+            building["site"]["building"]["latitude"] = site["latitude"]
+            building["site"]["building"]["longitude"] = site["longitude"]
+            dnac.sites.create_site(payload=building)
+        elif site["type"] == 'floor':
+            floor["site"]["floor"]["name"] = site["name"]
+            floor["site"]["floor"]["parentName"] = site["parentName"]
+            floor["site"]["floor"]["height"] = site["height"]
+            floor["site"]["floor"]["length"] = site["length"]
+            floor["site"]["floor"]["width"] = site["width"]
+            floor["site"]["floor"]["rfModel"] = site["rfModel"]
+            dnac.sites.create_site(payload=floor)
+
+ 
+if __name__ == "__main__":
+    deploy_sites(AREAS)
---
+++ cli_templates.py
@@ -0,0 +1,134 @@
+from dnacentersdk import api
+from datetime import datetime
+import yaml
+import time
+import sys
+import logging
+
+logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+
+dnac = api.DNACenterAPI()
+
+today = datetime.today()
+TSTAMP = today.strftime("%Y-%m-%dT%H-%M-%S")
+
+CONFIGURATION_TEMPLATES = ""
+with open('config/cli_templates.yaml', 'r') as file:
+    CONFIGURATION_TEMPLATES = yaml.safe_load(file)
+
+
+def validate_devices(configuration_templates):
+    for configuration_template in configuration_templates:
+        devices = configuration_template.get("devices", None)
+        if devices:
+            for device in devices:
+                dnac_response = dnac.devices.get_device_list(hostname=device.get("name"))
+                if not len(dnac_response.response):
+                    logger.error(f"Device {device.get('name')} does not exist")
+                    sys.exit(1)
+
+def has_defined_params(devices, params):
+    """
+    Checks if the template has params and if the devices has those params defined
+    """
+    template_params = []
+    for param in params:
+        template_params.append(param.get("parameterName"))
+    for device in devices:
+        if device.get("params", None) is None:
+            logger.error(f"Device {device.get('name')} does not have params defined.")
+            sys.exit(1)
+        for param in template_params:
+            if device.get("params").get(param, None) is None:
+                logger.error(f"Device {device.get('name')} does not have {param} defined.")
+                sys.exit(1)
+    return True
+
+
+
+def deploy_template(project, project_id, has_params):
+    devices = project.get("devices")
+    template_filename = project.get("file")
+    product_family = project.get("product_family")
+    device_types = []
+    for family in product_family:
+        device_types.append({"productFamily": family})
+
+    template_file = ""
+    with open(f"templates/{template_filename}", 'r') as file:
+        template_file = file.read()
+    template_params  = []
+    if has_params:
+        template_params = project.get("params")
+
+    compliance_template_task = dnac.configuration_templates.create_template(
+        project_id = project_id,
+        author="jenkins",
+        composite=False,
+        description=f"Jenkins deployment {TSTAMP}",
+        deviceTypes=device_types,
+        softwareType="IOS-XE",
+        softwareVariant="XE",
+        templateContent=template_file,
+        language="JINJA",
+        name=f"{project.get('name')}-{TSTAMP}",
+        templateParams=template_params
+    )
+
+    logger.info(f"Compliance project {project.get('name')}-{TSTAMP} task id is: {compliance_template_task.response.taskId}")
+    logger.info("Creating template. Sleeping for 60 seconds...")
+    time.sleep(10)
+
+    task = dnac.task.get_task_by_id(compliance_template_task.response.taskId)
+    template_id = task.response.data
+    
+    dnac.configuration_templates.version_template(comments="Jenkins Deployment", templateId=template_id)
+
+    logger.info("Versioning template. Sleeping for 10 seconds...")
+    time.sleep(10)
+
+    target_info = []
+    for device in devices:
+        if has_params:
+            target_info.append({"id": device.get("name"), 
+                                "type": "MANAGED_DEVICE_HOSTNAME",
+                                "params": device.get("params")})
+        else:
+            target_info.append({"id": device.get("name"), "type": "MANAGED_DEVICE_HOSTNAME"})
+
+    deploy_template_task = dnac.configuration_templates.deploy_template_v2(
+        forcePushTemplate=True,
+        isComposite=False,
+        templateId=template_id,
+        targetInfo=target_info
+    )
+
+    logger.info("Deploying template. Sleeping for 60 seconds...")
+    time.sleep(10)
+    task = dnac.task.get_task_by_id(deploy_template_task.response.taskId)
+    logger.info(f"Deployment status is: {task.response.progress}")
+
+def get_project(name):
+    projects = dnac.configuration_templates.get_projects(name=name)
+    if len(projects) > 0:
+        return projects[0].id
+    project_task = dnac.configuration_templates.create_project(name=name)
+    time.sleep(5)
+    project_task_data = dnac.task.get_task_by_id(project_task.response.taskId)
+    return project_task_data.response.data
+
+if __name__ == "__main__":
+    validate_devices(CONFIGURATION_TEMPLATES)
+    for configuration_template in CONFIGURATION_TEMPLATES:
+        project_id = get_project(configuration_template["project"])
+        if configuration_template.get("params"):
+            if has_defined_params(configuration_template.get("devices"),
+                                     configuration_template.get("params")):
+                logger.info("Deploying template with params...")
+                deploy_template(configuration_template, project_id, True)
+        else:
+            logger.info("Deploying template without params...")
+            deploy_template(configuration_template, project_id, False)
